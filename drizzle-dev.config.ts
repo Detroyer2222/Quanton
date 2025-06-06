abe278553e6a80ce2bcc3767493563f08cc50a73
Detroyer2222 <49248383+Detroyer2222@users.noreply.github.com>
2025-06-07 01:31:58 +0200

    Add production and development database configuration files and update package scripts

---
+++ drizzle-dev.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'drizzle-kit';
+
+if (!process.env.DEV_DATABASE_URL) throw new Error('DEV_DATABASE_URL is not set');
+
+export default defineConfig({
+	schema: './src/lib/server/db/schema.ts',
+	dialect: 'turso',
+	dbCredentials: {
+		authToken: process.env.DEV_DATABASE_AUTH_TOKEN,
+		url: process.env.DEV_DATABASE_URL
+	},
+	verbose: true,
+	strict: true
+});
