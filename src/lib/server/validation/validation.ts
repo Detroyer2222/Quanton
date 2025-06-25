5c43e55b04ab85a889c2ce16e68f621c46d9a61b
Detroyer2222 <49248383+Detroyer2222@users.noreply.github.com>
2025-06-25 16:49:42 +0200

    refactor: reorganize Pocketbase authentication code for clarity

--- src/lib/server/validation/validation.ts
+++ src/lib/server/validation/validation.ts
@@ -1,17 +1,20 @@
 import { PUBLIC_POCKETBASE_URL } from '$env/static/public';
-import { PRIVATE_POCKETBASE_VALIDATION_TOKEN } from '$env/static/private';
 import Pocketbase from 'pocketbase';
-import { type TypedPocketBase } from '$lib/types';
+import { Collections, type TypedPocketBase } from '$lib/types';
+import {
+	PRIVATE_POCKETBASE_VALIDATION_USER,
+	PRIVATE_POCKETBASE_VALIDATION_PASSWORD
+} from '$env/static/private';
 
 export async function authenticatePocketbase(): Promise<TypedPocketBase> {
 	const pb = new Pocketbase(PUBLIC_POCKETBASE_URL);
 
 	// eslint-disable-next-line @typescript-eslint/no-unused-vars
-	// const auth = await pb
-	//	.collection(Collections.Superusers)
-	//	.authWithPassword(PRIVATE_POCKETBASE_VALIDATION_USER, PRIVATE_POCKETBASE_VALIDATION_PASSWORD);
+	const auth = await pb
+		.collection(Collections.Superusers)
+		.authWithPassword(PRIVATE_POCKETBASE_VALIDATION_USER, PRIVATE_POCKETBASE_VALIDATION_PASSWORD);
 
-	pb.authStore.save(PRIVATE_POCKETBASE_VALIDATION_TOKEN);
+	// pb.authStore.save(PRIVATE_POCKETBASE_VALIDATION_TOKEN);
 	if (!pb.authStore.isValid) {
 		throw new Error('Pocketbase authentication failed. Please check your credentials.');
 	}
